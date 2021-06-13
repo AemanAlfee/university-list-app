30bdab44d0c455f08559b0e7a67a668edcbcd1d8
AemanAlfee <aalfee15@gmail.com>
2021-06-13 21:44:16 +0530

    updating test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+// We're using our own custom render function and not RTL's render
+// our custom utils also re-export everything from RTL
+// so we can import fireEvent and screen here as well
+import { render, act } from "./test-utils";
+import Universities from "./Universities";
+const saga = ({ dispatch, getState }) => (next) => (action) => {
+  if (typeof action === "function") {
+    return action(dispatch, getState);
+  }
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  return next(action);
+};
+
+const create = () => {
+  const store = {
+    getState: jest.fn(() => ({})),
+    dispatch: jest.fn()
+  };
+  const next = jest.fn();
+
+  const invoke = (action) => saga(store)(next)(action);
+
+  return { store, next, invoke };
+};
+
+it("Renders the app with initialState", () => {
+  act(() => {
+    render(<Universities />, { initialState: { user: "Redux User" } });
+  });
+});
+it("passes through non-function action", () => {
+  const { next, invoke } = create();
+  const action = { type: "TEST" };
+  invoke(action);
+  expect(next).toHaveBeenCalledWith(action);
+});
+it("passes dispatch and getState", () => {
+  const { store, invoke } = create();
+  invoke((dispatch, getState) => {
+    dispatch("FETCH_UNIVERSITIES");
+    getState();
+  });
+  expect(store.dispatch).toHaveBeenCalledWith("FETCH_UNIVERSITIES");
+  expect(store.getState).toHaveBeenCalled();
 });
